356739f674e767187254c267be758c3f9ed2a3b1
BlackJoe <73233199+DeityJoe@users.noreply.github.com>
2021-05-28 18:31:36 +0800

    Create w.sh

---
+++ w.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+
+mkdir "webp"
+root=$(pwd)
+dirs=$(ls)
+for dir in $dirs
+do
+  cd $dir
+  images=$(ls *.{png,jpg})
+  for image in $images
+  do
+    cwebp $image -q 90 -o $root/webp/${image%.*}.webp
+  done
+done
