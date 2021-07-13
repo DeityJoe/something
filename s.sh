5e97ff43eb0d5f6d1d6c7938ae3a7514b31d6d49
BlackJoe <73233199+DeityJoe@users.noreply.github.com>
2021-07-13 20:37:37 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -6,7 +6,7 @@ do
   for mid in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99
   do
     if [ ! -d "${folder}00000/${folder}${mid}000-${folder}${mid}999" ]; then
-    fclone copy gc:{0AC3AskGUtMDdUk9PVA}/${folder}00000/libgen.scimag${folder}${mid}000-${folder}${mid}999.zip ./
+    fclone copy gc:{0AC3AskGUtMDdUk9PVA}/${folder}00000/ ./
     unzip libgen.scimag${folder}${mid}000-${folder}${mid}999.zip -d ${folder}00000/${folder}${mid}000-${folder}${mid}999
     fclone copy ${folder}00000/${folder}${mid}000-${folder}${mid}999 gc:{}/${folder}00000/${folder}${mid}000-${folder}${mid}999 --transfers=16 --tpslimit 5000
     sleep 1s
